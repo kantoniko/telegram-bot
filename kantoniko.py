96efdcba1ec4b0001965f9396b04871006704818
Gabor Szabo <gabor@szabgab.com>
2023-06-14 23:27:33 +0300

    handle ke es messages

--- kantoniko.py
+++ kantoniko.py
@@ -3,6 +3,8 @@ import asyncio
 import telegram
 from telegram.ext import filters, Application, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 import os
+import re
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -19,6 +21,11 @@ async def help(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info(f"help")
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Yo so un bot. Avle kon me, por favor! https://kantoniko.com/")
 
+def lookup(word):
+    words = {
+        'kaza': 'house'
+    }
+    return words.get(word)
 
 async def echo(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
@@ -26,13 +33,30 @@ async def echo(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
     response = f"Resivi: '{text}'"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
+async def handle_message(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    logging.info(f"received: '{text}'")
+
+    match = re.search(r'^ke es (.*)', text)
+    if match:
+        word = match.group(1)
+        response = lookup(word)
+        if not response:
+            response = f"No se ke es '{word}'"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+
 async def translate(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
     if len(context.args) > 2:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="No puedo tradusir ekspresiones kon mas de una palavra!")
         return
+    if len(context.args) == 0:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Tenemos de tener una palavra: /traduse kaza")
+        return
+
     text = context.args[0]
-    if text == 'kaza':
-        translation = 'house'
+    translation = lookup(text)
+    if translation:
         await context.bot.send_message(chat_id=update.effective_chat.id, text=translation)
         return
 
@@ -44,7 +68,7 @@ async def translate(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE)
 async def post_init(application: Application) -> None:
     logging.info(f"post_init")
     #await application.bot.set_my_commands([('start', 'Starts the bot'), ('traduse', 'Tradusir una palavara')])
-    await application.bot.set_my_commands([('ayuda', 'Ayudame!'), ('traduse', 'Tradusir una palavara')])
+    await application.bot.set_my_commands([('ayuda', 'Ayudame!')]) #, ('traduse', 'Tradusir una palavara')])
 
 
 if __name__ == '__main__':
@@ -54,12 +78,14 @@ if __name__ == '__main__':
 
     application = ApplicationBuilder().token(token).post_init(post_init).build()
     #echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message)
     #start_handler = CommandHandler('start', start)
     help_handler = CommandHandler('ayuda', help)
     translate_handler = CommandHandler('traduse', translate)
 
     #application.add_handler(start_handler)
     #application.add_handler(echo_handler)
+    application.add_handler(message_handler)
     application.add_handler(help_handler)
     application.add_handler(translate_handler)
     #print(dir(application))
