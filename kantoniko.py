fc358b991c6c2a2a19e59f9ded4b2caccdd4fab7
Gabor Szabo <gabor@szabgab.com>
2023-06-14 20:35:01 +0300

    talk more

--- kantoniko.py
+++ kantoniko.py
@@ -1,29 +1,55 @@
+import logging
 import asyncio
 import telegram
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-token = os.environ.get('token')
-if not token:
-    xit("No token found")
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-async def main():
-    bot = telegram.Bot(token)
-    async with bot:
-        about_me = await bot.get_me()
-        # User(can_join_groups=True, can_read_all_group_messages=False, first_name='Kantoniko', id=5972296076, is_bot=True, supports_inline_queries=False, username='kantoniko_bot')
-        updates = await bot.get_updates()
-        #print(updates)
-        #for update in updates:
-        #    print(update)
-        #    print()
-        echar_lashon_id = -810127428
-        #first_name = updates[0].message.from_user.first_name
-        #user_id = updates[0].message.from_user.id
-        #await bot.send_message(text=f'Hi {first_name}', chat_id=user_id)
-        #await bot.send_message(text=f'Yo so Kantoniko', chat_id=echar_lashon_id)
+async def start(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Yo so un bot. Avle kon me, por favor!")
+
+async def echo(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    logging.info(f"received: '{text}'")
+    response = f"Resivi: '{text}'"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+async def translate(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+    if len(context.args) > 2:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="No puedo tradusir ekspresiones kon mas de una palavra!")
+        return
+    text = context.args[0]
+    if text == 'kaza':
+        translation = 'house'
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=translation)
+        return
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"No konosko la palavar '{text}'")
+
+
+#        echar_lashon_id = -810127428
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    token = os.environ.get('token')
+    if not token:
+        exit("No token found")
+
+    application = ApplicationBuilder().token(token).build()
+
+    #echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    start_handler = CommandHandler('start', start)
+    translate_handler = CommandHandler('traduse', translate)
+
+    application.add_handler(start_handler)
+    #application.add_handler(echo_handler)
+    application.add_handler(translate_handler)
+
+    application.run_polling()
+
